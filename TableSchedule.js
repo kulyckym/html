6276faf36b1f412a76acb0a6bf089ec6c34b5698
kulyckym <michael.kulycky19@alumni.loyola.ca>
2018-02-09 09:19:24 -0500

    Update TableSchedule.js

--- TableSchedule.js
+++ TableSchedule.js
@@ -1,1 +1,24 @@
 <script type="text/javascript">
+var expr = 'switchClass'
+switch (expr){
+	case 'programming'
+		
+		break;
+	case 'history'
+		break;
+	case 'NS math'
+		break;
+	case 'english'
+		break;
+	case 'science'
+		break;
+	case 'french'
+		break;
+	case 'music'
+		break;
+	case 'speech'
+		break;
+	case 'religion'
+		break;
+	case 'physical education'
+		break;
